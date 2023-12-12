d393fa836380886042028463dc2f9a4aa9d3b7dc
simonostini <simonostini@gmail.com>
2023-12-12 21:29:02 +0100

    updated time output

--- src/app/components/work-item.tsx
+++ src/app/components/work-item.tsx
@@ -9,13 +9,19 @@ export default function WorkItem(work: WorkPost) {
         <h2 className="font-medium text-xl mb-1">{work.title}</h2>
         <div className="mb-8 text-sm text-neutral-600">
           {work.url && (
-            <a className="mr-4" href={work.url.href}>
+            <a className="mr-4 underline" href={work.url.href} target="_blank">
               {work.url.hostname}
             </a>
           )}
           <span>
-            {`${work.startDate.toLocaleDateString()} — ${
-              work.endDate?.toLocaleDateString() || "present"
+            {`${
+              work.startDate.getMonth() + 1
+            }.${work.startDate.getFullYear()} — ${
+              (work.endDate &&
+                `${
+                  work.endDate.getMonth() + 1
+                }.${work.endDate.getFullYear()}`) ||
+              "present"
             }`}
           </span>
         </div>
