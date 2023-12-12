f69644c2a9da1da208b84f934fb5683db4e4867d
Simon Ostini <simon.ostini@gmg.biz>
2023-12-12 14:40:54 +0100

    updated layout spacings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <section>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         test
       </div>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         test
       </div>
-    </main>
+    </section>
   );
 }
