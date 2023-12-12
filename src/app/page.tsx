827338ad53e31db04d1d3acf4ea650a43aa53c99
Simon Ostini <simon.ostini@gmg.biz>
2023-12-12 15:02:14 +0100

    added lib directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
     <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        👋 {`I'm Simon`}
+      </h1>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         test
       </div>
