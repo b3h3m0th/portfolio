88bad0b240dfe7b9ee67a847a62e0000c9541419
simonostini <simonostini@gmail.com>
2023-12-19 19:18:09 +0100

    added background

--- src/app/components/cursor.tsx
+++ src/app/components/cursor.tsx
@@ -8,13 +8,13 @@ export default function Cursor() {
 
   return (
     <motion.div
-      className="z-[-1] left-0 top-0 w-48 h-48 invisible md:visible md:fixed -translate-x-1/2 -translate-y-1/2 rounded-full pointer-events-none"
+      className="z-[-1] left-0 top-0 w-0 h-0 md:w-48 md:h-48 invisible md:visible fixed -translate-x-1/2 -translate-y-1/2 rounded-full pointer-events-none"
       style={{
-        background: "linear-gradient(90deg,#0ff 1.5%,#0f0 2%,#000 100%)",
+        background: "linear-gradient(90deg,#0ff 1.5%,#000 2%,#000 100%)",
         filter: "blur(150px)",
       }}
       animate={{ x: x - 96, y: y - 96 }}
-      transition={{ ease: "linear", duration: 0, type: "spring" }}
+      transition={{ ease: "linear", duration: 2 }}
     ></motion.div>
   );
 }
