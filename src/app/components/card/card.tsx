801f97001e3637d0226014803952559f9f8a606c
simonostini <simonostini@gmail.com>
2024-01-23 23:07:17 +0100

    updated not found

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { CardScene } from "./card-scene";
 import { useState } from "react";
