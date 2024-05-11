93bcb9be7b33c818dd9618a969f6c3b75b25bce9
simonostini <simonostini@gmail.com>
2024-05-11 02:23:00 +0200

    udpate

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ const year = new Date().getFullYear();
 export function Footer() {
   return (
     <div className="mt-20 mb-8 flex justify-center">
-      <span>Made with 🖤 by Simon Ostini &copy; {year}</span>
+      <span>Made with 🤍 by Simon Ostini &copy; {year}</span>
     </div>
   );
 }
